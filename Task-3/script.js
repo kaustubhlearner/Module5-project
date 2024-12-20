afb3ec06554bc316c4f80a4f86acd3be0c765f94
kaustubhlearner <kaustubhpandey81788@gmail.com>
2024-12-20 12:24:38 +0530

    task-3

---
+++ Task-3/script.js
@@ -0,0 +1,10 @@
+let maindiv = document.createElement("div")
+document.body.append(maindiv)
+maindiv.classList.add("bimage")
+
+
+let header = document.createElement("h1")
+maindiv.append(header)
+header.innerText= "Code is more than some bytes in a file"
+header.style.color = "white"
+
