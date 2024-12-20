036eb097f3cd68cdcc1bd6a73f184fa0c5918006
kaustubhlearner <kaustubhpandey81788@gmail.com>
2024-12-20 15:54:16 +0530

    task-5

---
+++ Task-5/script.js
@@ -0,0 +1,8 @@
+// let update = document.getElementById("image")
+
+
+function change(){
+   
+    let update = document.getElementById("image")
+    update.src=" https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/65532/happy-emoji-clipart-md.png"
+}
